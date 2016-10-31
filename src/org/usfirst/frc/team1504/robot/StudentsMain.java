0f3561934480ec44cbbf7ae3d7796d3f64d19e25
ZovcIfzm <alexruan2001@gmail.com>
2016-10-30 21:40:43 -0400

    Let there be code!
    Initial commit
    
    And so it was on this day, that the branch Students of Robot_Framework
    was born into this world. It is today in which Students hath spoken,
    "Hello World" and graced us with its presense. Oh Students! How will you
    grace the world? Will you create code to last the test of time?

---
+++ src/org/usfirst/frc/team1504/robot/StudentsMain.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team1504.robot;
+
+import org.usfirst.frc.team1504.robot.Update_Semaphore.Updatable;
+
+public class StudentsMain implements Updatable {
+
+	@Override
+	public void semaphore_update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
+//where the main stuff is.
