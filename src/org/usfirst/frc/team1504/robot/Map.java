ced960a7f1e8c00cd55d67c0c51bd07ddc130106
TheOneTrueCec <cec@conklinsystems.com>
2016-11-14 18:30:24 -0500

    Update Map.java

--- src/org/usfirst/frc/team1504/robot/Map.java
+++ src/org/usfirst/frc/team1504/robot/Map.java
@@ -91,6 +91,9 @@ public class Map {
 	public static final int PNEUMATICS_HIGHSIDE_PORT = 0;
 	public static final int PNEUMATICS_LOWSIDE_PORT = 1;
 	
+	public static final int PNEUMATICS_PISTON_PORT_ONE = 2;
+	public static final int PNEUMATICS_PISTON_PORT_TWO = 3;
+	
 /**
  * Logger stuff
  */
